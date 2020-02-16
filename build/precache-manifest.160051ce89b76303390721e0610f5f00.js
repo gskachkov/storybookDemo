954c66995b7fcd0ab69b06113f2bf7827b73460e
Oleksandr Skachkov <gskachkov@gmail.com>
2020-02-16 13:34:02 +0200

    Static version

---
+++ build/precache-manifest.160051ce89b76303390721e0610f5f00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66d32e26a8cdd2799e03a32c57566a5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90380443a4d4736d6c58",
+    "url": "/static/css/main.47b4b43c.chunk.css"
+  },
+  {
+    "revision": "42fbe99192a91c1d232b",
+    "url": "/static/js/2.a62183e4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a62183e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90380443a4d4736d6c58",
+    "url": "/static/js/main.0df6cad4.chunk.js"
+  },
+  {
+    "revision": "2db3a2c1ba58912f4f56",
+    "url": "/static/js/runtime-main.da229d6d.js"
+  }
+]);
